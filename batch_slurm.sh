3bd8a5b0c76395107d841bb82823a1c660c86746
Dixi Yao <pspo-qfl@163.com>
2026-03-26 15:58:20 -0500

    Update batch_slurm.sh

--- batch_slurm.sh
+++ batch_slurm.sh
@@ -5,9 +5,9 @@
 #SBATCH --gres=gpu:a100:4
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks=1
-#SBATCH --array=0
+#SBATCH --array=1-119
 #SBATCH --output=logs/%x_%A_%a.out
-#SBATCH --error=logs/%x_%A_%a.err
+#SBATCH --error=logs/%x_%A_%a.txt
 
 set -euo pipefail
 
